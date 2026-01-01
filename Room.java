45983237ae13f7fd9b7e77734214795125721c6c
LuoMichael1 <136616479+LuoMichael1@users.noreply.github.com>
2026-01-01 04:20:43 -0500

    New room background
    Original design :D

--- Room.java
+++ Room.java
@@ -5,7 +5,7 @@ import javax.swing.ImageIcon;
 
 public class Room extends Entities {
     
-    static ImageIcon roomImage = new ImageIcon("Assets/Images/roombg1.png");
+    static ImageIcon roomImage = new ImageIcon("Assets/Images/roombg2.png");
     static final int roomSizeX = 1920;
     static final int roomSizeY = 1080;
 
