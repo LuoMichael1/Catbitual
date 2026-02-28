9cb576b198b24b23ecdd5037e329cf23597185e7
LuoMichael1 <136616479+LuoMichael1@users.noreply.github.com>
2026-02-27 23:13:04 -0500

    improved appearence of pet store

--- PetStore.java
+++ PetStore.java
@@ -1,3 +1,6 @@
+// menu which allows user to purchase food and furniture for their cat!
+
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.Image;
@@ -7,6 +10,7 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
@@ -16,28 +20,29 @@ public class PetStore extends JScrollPane implements ActionListener{
     
     private ArrayList<JButton> buttons = new ArrayList<JButton>();
     private ArrayList<ImageIcon> images = new ArrayList<ImageIcon>();
-    private Menu m;
     private ArrayList<String> imageFile = new ArrayList<String>(); 
     private double previewImageWidth = 0;
     private double previewImageHeight = 0;
     private double previewImageScale = 1;
     private int SCROLLSPEED = (int)(8*Main.scaleY);
+    private Menu m;
+    private JPanel scrollPanel = new JPanel();
 
     public PetStore(Menu m) {
         this.m = m;
 
-        this.setBorder(null);
-        JPanel scrollPanel = new JPanel();
-        scrollPanel.setLayout(new GridLayout(0,2, 10, 10));
+        // creates a small margin around the scrollable part of the menu
+        this.setBorder(BorderFactory.createMatteBorder(0,(int)(30*Main.scaleX),(int)(30*Main.scaleX),(int)(30*Main.scaleX),Color.white));
+        
+        scrollPanel.setLayout(new GridLayout(0,2, (int)(10*Main.scaleX), (int)(10*Main.scaleX)));
         scrollPanel.setBackground(Color.WHITE);
         
         // Make scrolling faster
-        this.getVerticalScrollBar().setUnitIncrement(SCROLLSPEED); // default is around 1-10
+        this.getVerticalScrollBar().setUnitIncrement(SCROLLSPEED);
 
-
+        // get image files
         try {
             Scanner filesc = new Scanner(new File("Assets/Images/Furniture/Catalog.txt"));
-
             while (filesc.hasNextLine()) {
                 imageFile.add(filesc.nextLine());
             }
@@ -47,29 +52,33 @@ public class PetStore extends JScrollPane implements ActionListener{
             System.out.println(e);
         }   
 
-
+        // display the items
         for (int i=0; i<imageFile.size(); i++) {
-            JPanel j = new JPanel();
+            JPanel j = new RoundedPanel();
+            j.setLayout(new BorderLayout());
             ImageIcon img = new ImageIcon("Assets/Images/Furniture/" + imageFile.get(i));
             
             images.add(img);
 
-
-            //resize image so that they are half the size of the menu, while not being stretched
+            // resize image so that they are half the size of the menu, while not being stretched
             if (images.get(i).getIconWidth() > images.get(i).getIconHeight()) {
-                previewImageScale = Main.width/5.0/images.get(i).getIconWidth();
+                previewImageScale = Main.width/6.5/images.get(i).getIconWidth();
             }
             else {
-                previewImageScale = Main.width/5.0/images.get(i).getIconHeight();
+                previewImageScale = Main.width/6.5/images.get(i).getIconHeight();
             }
             previewImageWidth = images.get(i).getIconWidth()*previewImageScale;
             previewImageHeight = images.get(i).getIconHeight()*previewImageScale;
 
             JButton b = new JButton(new ImageIcon(images.get(i).getImage().getScaledInstance((int)previewImageWidth, (int)previewImageHeight, Image.SCALE_SMOOTH)));
+            b.setContentAreaFilled(false);
+            b.setBorderPainted(false);
             b.addActionListener(this);
+
             j.setBackground(new Color(240, 240, 240));
-            j.add(b);
+            j.add(b, BorderLayout.CENTER);
             
+
             buttons.add(b);
             scrollPanel.add(j);
         }
@@ -78,14 +87,12 @@ public class PetStore extends JScrollPane implements ActionListener{
     }
 
     public void actionPerformed(ActionEvent e) {
-        System.out.println("furn ?");
         for (int i=0; i<buttons.size(); i++) {
             if (buttons.get(i) == e.getSource()) {
                 // create an entity in the room
                 Furniture furn = new Furniture(images.get(i));
                 Menu.addEntity(furn);
-                m.repaint();//(getGraphics());
-                System.out.println("furn created");
+                m.repaint();
             }
         }
     }
