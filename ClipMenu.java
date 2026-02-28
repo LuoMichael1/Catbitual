7dd46091d177aaed481cdfe28ad42bd4ef27b4d7
LuoMichael1 <136616479+LuoMichael1@users.noreply.github.com>
2026-02-28 09:44:47 -0500

    Update ClipMenu.java

--- ClipMenu.java
+++ ClipMenu.java
@@ -4,6 +4,8 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 
 import javax.swing.BorderFactory;
+
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 
