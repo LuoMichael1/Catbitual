ad67fa2d515a68a94362ec81655502ff67493d57
LuoMichael1 <136616479+LuoMichael1@users.noreply.github.com>
2026-02-28 13:21:11 -0500

    prices are now displayed

--- PetStore.java
+++ PetStore.java
@@ -2,8 +2,12 @@
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -13,8 +17,11 @@ import java.util.Scanner;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.OverlayLayout;
 
 public class PetStore extends JScrollPane implements ActionListener{
     
@@ -30,14 +37,16 @@ public class PetStore extends JScrollPane implements ActionListener{
 
     public PetStore(Menu m) {
         this.m = m;
-
-        // creates a small margin around the scrollable part of the menu
-        this.setBorder(BorderFactory.createMatteBorder(0,(int)(30*Main.scaleX),0,(int)(30*Main.scaleX),Color.white));
+        this.setBorder(null);
+       
         
         // adds margin between each displayed item
         scrollPanel.setLayout(new GridLayout(0,2, (int)(15*Main.scaleX), (int)(15*Main.scaleX)));
         scrollPanel.setBackground(Color.WHITE);
         
+        // creates a small margin around the scrollable part of the menu
+        scrollPanel.setBorder(BorderFactory.createMatteBorder(0,(int)(30*Main.scaleX),30,(int)(30*Main.scaleX),Color.white));
+
         // Make scrolling faster
         this.getVerticalScrollBar().setUnitIncrement(SCROLLSPEED);
 
@@ -56,7 +65,9 @@ public class PetStore extends JScrollPane implements ActionListener{
         // display items
         for (int i=0; i<imageFile.size(); i++) {
             JPanel j = new RoundedPanel();
-            j.setLayout(new BorderLayout());
+            j.setLayout(new OverlayLayout(j));
+            j.setBackground(new Color(240, 240, 240));
+            
             ImageIcon img = new ImageIcon("Assets/Images/Furniture/" + imageFile.get(i));
             
             images.add(img);
@@ -73,15 +84,48 @@ public class PetStore extends JScrollPane implements ActionListener{
             previewImageHeight = images.get(i).getIconHeight()*previewImageScale;
 
             // makes clicking on a display functional
+            JPanel bp = new RoundedPanel();
+            bp.setLayout(new BorderLayout());
             JButton b = new JButton(new ImageIcon(images.get(i).getImage().getScaledInstance((int)previewImageWidth, (int)previewImageHeight, Image.SCALE_SMOOTH)));
             b.setContentAreaFilled(false);
             b.setBorderPainted(false);
             b.addActionListener(this);
+            bp.add(b);
+           
+            // display prices
+            RoundedPanel p = new RoundedPanel();
+            p.setAlignmentX(1.0f);  // left
+            p.setAlignmentY(0.0f);  // bottom
+            p.setBackground(new Color(0,0,0,0));
+            //p.setBackground(Color.YELLOW);
+            p.setLayout(new GridBagLayout());
 
-            j.setBackground(new Color(240, 240, 240));
-            j.add(b, BorderLayout.CENTER);
+            RoundedPanel p2 = new RoundedPanel();
+            p2.setMaximumSize(new Dimension(200, 50));
+            //p2.setBorder(BorderFactory.createMatteBorder((int)(30*Main.scaleX),(int)(30*Main.scaleX),(int)(100*Main.scaleX),(int)(30*Main.scaleX), Color.blue));//new Color(0,0,0,0)));
+            p2.setBackground(Color.WHITE);
+            //p.add(p2);
             
+            JLabel jl = new JLabel("100 clams");
+            jl.setFont(FontMaker.p);
+            p2.add(jl);
 
+            // GridBagConstraints to anchor bottom-left
+            GridBagConstraints g = new GridBagConstraints();
+            g.gridx = 0;
+            g.gridy = 0;
+            g.anchor = GridBagConstraints.LAST_LINE_START; // left alignment
+            g.fill = GridBagConstraints.NONE;   // allow resizing horizontally
+            g.weightx = 1; 
+            g.weighty = 1;                         
+            g.ipadx = 30;
+            g.ipady = 5;
+            g.insets = new Insets(15, 15, 15, 15); // nudges the price to the bottom left corner
+            p.add(p2, g);
+            
+            j.add(p);  // add overlay FIRST
+            j.add(bp);  // add image SECOND
+            
             buttons.add(b);
             scrollPanel.add(j);
         }
